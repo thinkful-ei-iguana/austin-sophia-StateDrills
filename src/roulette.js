107e1a1c7cfb5cfa111cb9ad38bd6c0718397585
Sophia Koeut <koeutsophia@gmail.com>
2019-11-13 14:34:59 -0800

    roulette gun almost thereee!

--- src/roulette.js
+++ src/roulette.js
@@ -2,20 +2,29 @@ import React from 'react';
 
 export default class RouletteGun extends React.Component {
     static defaultProps = {
-        bulletInChamber: 8, 
+        bulletInChamber: 8,
     }
-    state ={
+
+    state = {
         chamber: null,
         spinningTheChamber: false
     };
 
     handleClick = e => {
         this.setState({
-            spinningTheChamber: !this.spinningTheChamber
+            spinningTheChamber: true
         });
+
+        setTimeout(function () {
+            const randomNumber = Math.ceil(Math.random() * 8);
+            this.setState({
+                chamber: randomNumber,
+                spinningTheChamber: false
+            });
+        }, 2 * 1000);
     };
 
-    rouletteSpin () {
+    rouletteSpin() {
         // let count = this.state.count;
         if (this.spinningTheChamber === true) {
             return 'Spinning the chamber and pulling the trigger!';
@@ -26,20 +35,15 @@ export default class RouletteGun extends React.Component {
         }
     }
 
-    
-   
-    componentWillUnmount () {
-        setTimeout =(function () {
-            Math.ceil(Math.random() * 8);
-        }, 2*1000);
-         clearTimeout(setTimeout);
+    componentWillUnmount() {
+        clearTimeout(setTimeout);
     }
 
     render() {
         return (
             <div>
                 <button onClick={this.handleClick}>Spin Trigger!</button>
-        {this.state.spinningTheChamber && <p>{this.rouletteSpin()}</p>}
+                {this.state.spinningTheChamber && <p>{this.rouletteSpin()}</p>}
             </div>
         );
     }
